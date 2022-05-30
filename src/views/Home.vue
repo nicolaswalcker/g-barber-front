80ab82fb94770c29ab99045c61b5178f4ac97c03
Nicolas Walcker <walcker_@outlook.com>
2022-05-30 10:01:53 -0300

    chore: remove unecessary files [sc-30]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,11 @@
 <template>
-  <div class="home">
-    <div class="home">
-      <!-- Using a SVG -->
-      <SvgElement
-        type="logo"
-        name="VueLogo"
-        title="Vue.js"
-      />
-
-      <HelloWorld msg="Welcome to Your Vue.js App" />
-    </div>
-  </div>
+  <div class="home" />
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
   export default {
     name: 'Home',
     components: {
-      HelloWorld
     }
   };
 </script>
