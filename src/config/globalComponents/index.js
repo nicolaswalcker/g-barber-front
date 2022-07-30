3d29a8e7c2e78da5f763caaf890fef4af41dffc9
Nicolas Walcker <walcker_@outlook.com>
2022-07-29 22:25:42 -0300

    chore: add feather icons call and new border radius

--- src/config/globalComponents/index.js
+++ src/config/globalComponents/index.js
@@ -1,3 +1,4 @@
 import Vue from 'vue';
 // Components
 Vue.component('SvgElement', () => import('@/components/base/svg/SvgElement.vue'));
+Vue.component('ButtonElement', () => import('@/components/ButtonElement.vue'));
