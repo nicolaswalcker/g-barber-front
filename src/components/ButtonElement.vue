36a583067502bd7fa891210b67e695631ef1df7f
Nicolas Walcker <walcker_@outlook.com>
2022-07-29 22:24:52 -0300

    feat: create button and input components

---
+++ src/components/ButtonElement.vue
@@ -0,0 +1,68 @@
+<template>
+  <button
+    class="button"
+    :class="`button${getButtonState}`"
+    :disabled="!buttonActive"
+    @click="handleButtonAction"
+  >
+    <p class="button__text">
+      {{ buttonText }}
+    </p>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'ButtonElement',
+    props: {
+      buttonText: {
+        type: String,
+        required: true
+      },
+      buttonActive: {
+        type: Boolean,
+        default: true
+      }
+    },
+    computed: {
+      getButtonState(){
+        return this.buttonActive ? '--primary' : '--disabled';
+      }
+    },
+    methods: {
+      handleButtonAction(){
+        this.$emit('button-action');
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+.button {
+  min-width: 21.25rem;
+  min-height: 3.125rem;
+  border-radius: 0.625rem;
+  border: none;
+  background-color: $orange;
+
+  &__text {
+    font-weight: 500;
+    font-size: 1rem;
+    color: $background;
+  }
+
+  &--primary {
+    cursor: pointer;
+    transition: all ease 0.2s;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
+  &--disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+}
+</style>
---
+++ src/components/InputElement.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="input">
+    <component
+      :is="returnInputIcon"
+      size="1.5x"
+      class="input__icon"
+    />
+    <input
+      :type="inputType"
+      class="input__field"
+      :placeholder="inputPlaceholder"
+    >
+  </div>
+</template>
+
+<script>
+import { LockIcon, MailIcon, UserIcon } from 'vue-feather-icons';
+export default {
+  name: 'InputElement',
+  components: {
+    LockIcon,
+    MailIcon,
+    UserIcon,
+  },
+  props: {
+    inputType: {
+      type: String,
+      default: 'text',
+    },
+    inputPlaceholder: {
+      type: String,
+      default: 'Email',
+    },
+    inputIcon: {
+      type: String,
+      default: 'mail',
+    },
+  },
+
+  computed: {
+    returnInputIcon(){
+      switch (this.inputIcon) {
+        case 'lock':
+          return 'LockIcon';
+        case 'mail':
+          return 'MailIcon';
+        case 'user':
+          return 'UserIcon';
+        default:
+          return 'MailIcon';
+      }
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.input {
+  color: $gray-hard;
+  position: relative;
+
+  &__field {
+    border-radius: $border-4;
+    min-height: 60px;
+    width: 100%;
+    background: $inputs;
+    border: none;
+    outline: none;
+    padding: 0 3rem;
+    color: $gray-hard;
+    font-weight: 500;
+  }
+
+  &__icon {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 1%;
+  }
+}
+</style>
