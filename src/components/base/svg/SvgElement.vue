a74e1c5f8cc213707fc14060ce2790ca92c57f7d
Nicolas Walcker <walcker_@outlook.com>
2022-08-07 22:10:34 -0300

    feat: add g-barber logo

--- src/components/base/svg/SvgElement.vue
+++ src/components/base/svg/SvgElement.vue
@@ -66,6 +66,8 @@ export default {
       switch (name) {
         case 'logo-VueLogo':
           return this.defaultData('261.76', '226.69', '0 0 261.76 226.69');
+        case 'logo-GBarberLogo':
+          return this.defaultData('231', '134', '0 0 231 134');
         default:
           return this.defaultData('20');
       }
