b3e248a3bf9add086fdc9c4f01ad4a1d57bd7a0c
Nicolas Walcker <walcker_@outlook.com>
2022-08-07 22:09:21 -0300

    chore: add favicon and little adjusts

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  extends: "stylelint-config-standard",
-  plugins: ["stylelint-scss"],
+  extends: 'stylelint-config-standard',
+  plugins: ['stylelint-scss'],
   rules: {
-    "at-rule-no-unknown": null,
-    "scss/at-rule-no-unknown": true,
-    "selector-pseudo-element-no-unknown": [
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'selector-pseudo-element-no-unknown': [
       true,
       {
-        ignorePseudoElements: ["v-deep"],
+        ignorePseudoElements: ['v-deep'],
       },
     ],
   },
-  ignoreFiles: ["src/assets/sass/base/_normalize.scss"],
+  ignoreFiles: ['src/assets/sass/base/_normalize.scss'],
 };
