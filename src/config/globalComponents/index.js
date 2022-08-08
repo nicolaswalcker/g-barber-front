5874739722b6fad400ba31b7c58721e8008162b4
Nicolas Walcker <walcker_@outlook.com>
2022-08-07 22:09:43 -0300

    fix: remove button for auto import

--- src/config/globalComponents/index.js
+++ src/config/globalComponents/index.js
@@ -1,4 +1,3 @@
 import Vue from 'vue';
 // Components
 Vue.component('SvgElement', () => import('@/components/base/svg/SvgElement.vue'));
-Vue.component('ButtonElement', () => import('@/components/ButtonElement.vue'));
